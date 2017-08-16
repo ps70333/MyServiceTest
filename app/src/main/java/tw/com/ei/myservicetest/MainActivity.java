9726daf9d79488cf940824ed364d003c58eb0208
ps70333 <ps70333@gmail.com>
2017-08-16 21:25:24 +0800

    B

--- app/src/main/java/tw/com/ei/myservicetest/MainActivity.java
+++ app/src/main/java/tw/com/ei/myservicetest/MainActivity.java
@@ -1,47 +1,107 @@
 package tw.com.ei.myservicetest;
 
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
-    public MyServiceConnection myServiceConnection;
+    private MyServiceConnection myServiceConnection;
+    private SeekBar seekBar;
+    private MyReceiver myReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //myServiceConnection=new MyServiceConnection();
-        //Intent it=new Intent(MainActivity.this,MyService.class);
-        //bindService(it,myServiceConnection, Context.BIND_AUTO_CREATE);
+
+        myReceiver = new MyReceiver();
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
+
+        IntentFilter filter = new IntentFilter("simon");
+        registerReceiver(myReceiver, filter);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser){
+                    Intent it = new Intent(MainActivity.this, MyService.class);
+                    it.putExtra("progress", progress);
+                    startService(it);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+//        Intent it = new Intent(MainActivity.this, MyService.class);
+//        myServiceConnection = new MyServiceConnection();
+//        bindService(it,myServiceConnection, Context.BIND_AUTO_CREATE);
+
     }
-    public class MyServiceConnection implements ServiceConnection{
+
+    @Override
+    public void finish() {
+        unregisterReceiver(myReceiver);
+        super.finish();
+    }
+
+    private class MyReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int len = intent.getIntExtra("len", -1);
+            int now = intent.getIntExtra("now", -1);
+            if (len >= 0 ){
+                seekBar.setMax(len);
+            }
+            if (now >= 0){
+                seekBar.setProgress(now);
+            }
+        }
+    }
+
+
+    private class MyServiceConnection implements ServiceConnection {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
         }
-
         @Override
         public void onServiceDisconnected(ComponentName name) {
-
         }
     }
-    public void start(View v){
-        Intent it=new Intent(MainActivity.this,MyService.class);
+
+    public void start(View view){
+        Intent it = new Intent(MainActivity.this, MyService.class);
+        it.putExtra("isStart", true);
         startService(it);
     }
-    public void pause(View v){
-        Intent it=new Intent(MainActivity.this,MyService.class);
+    public void pause(View view){
+        Intent it = new Intent(MainActivity.this, MyService.class);
+        it.putExtra("isPause", true);
         startService(it);
     }
-    public void stop(View v){
-        Intent it=new Intent(MainActivity.this,MyService.class);
+    public void stop(View view){
+        Intent it = new Intent(MainActivity.this, MyService.class);
         stopService(it);
     }
-    public void unbind(View v){
-        //unbindService(MyServiceConnection);
+    public void test(View view){
+        unbindService(myServiceConnection);
     }
+
 }
--- app/src/main/java/tw/com/ei/myservicetest/MyService.java
+++ app/src/main/java/tw/com/ei/myservicetest/MyService.java
@@ -9,6 +9,8 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  manifest要新增這個serivce
@@ -16,37 +18,80 @@ import java.io.IOException;
  */
 
 public class MyService extends Service {
-    public MediaPlayer player;
+    private MediaPlayer player;
+    private Timer timer;
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        Log.i("simon","onBind");
+        Log.i("simon", "onBind");
         return null;
     }
+
     @Override
     public void onCreate() {
         super.onCreate();
-        player=MediaPlayer.create(this,R.raw.test);
-        player.start();
+        timer = new Timer();
+        player = MediaPlayer.create(this,R.raw.test);
 
-        Log.i("simon","onCreate");
+        int len = player.getDuration();
+        Intent it = new Intent("simon"); // action = "simon"
+        it.putExtra("len", len);
+        sendBroadcast(it);
+
+        player.start();
+        timer.schedule(new PlayTask(), 0, 200);
+
+    }
+
+    private class PlayTask extends TimerTask {
+        @Override
+        public void run() {
+            if (player != null && player.isPlaying()){
+                int now = player.getCurrentPosition();
+                Intent it = new Intent("simon"); // action = "simon"
+                it.putExtra("now", now);
+                sendBroadcast(it);
+            }
+        }
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i("simon","onStartCommand");
+        boolean isStart = intent.getBooleanExtra("isStart", false);
+        boolean isPause = intent.getBooleanExtra("isPause", false);
+        int progress = intent.getIntExtra("progress", -1);
+
+        if (progress>=0) {
+            player.seekTo(progress);
+        }else if (isPause) {
+            if (player != null && player.isPlaying()){
+                player.pause();
+            }else{
+                player.start();
+            }
+        }
+
         return super.onStartCommand(intent, flags, startId);
     }
+
     @Override
     public boolean onUnbind(Intent intent) {
-        Log.i("simon","onUnbind");
+        Log.i("simon", "onUnbind");
         return super.onUnbind(intent);
     }
+
     @Override
     public void onDestroy() {
-        Log.i("simon","onDestroy");
         super.onDestroy();
+        if (timer != null){
+            timer.cancel();
+            timer.purge();
+            timer = null;
+        }
+        if (player != null ){
+            player.stop();
+        }
     }
-
 
 }
